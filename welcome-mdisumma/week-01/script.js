c2fe26d8a5951ac678520bd5f4a43155e9268027
Marco Di Summa <mdisumma@yahoo.com>
2021-01-25 16:42:52 +0000

    week-01

---
+++ welcome-mdisumma/week-01/script.js
@@ -0,0 +1,70 @@
+// declaring variable 
+var myVariable;
+
+console.log ("the value of my variable is " + myVariable);
+
+// assigning a value
+myVariable = 32;
+
+console.log ("the value of my variable is " + myVariable);
+
+//using let & const
+let studentName = " Marco Di Summa"
+const COURSE = " JavaScript "
+
+console.log ("student name" + studentName);
+console.log ("course name" + COURSE);
+
+// operator & concatenation
+var a = 2;
+var b = 4;
+var sum = a*b;
+
+console.log ("operators " + a + " * " + b + " = " + sum);
+
+var sum = a+b;
+console.log ("operators " + a + " + " + b + " = " + sum);
+
+var sum = a-b;
+console.log ("operators " + a + " - " + b + " = " + sum);
+
+var sum = a/b;
+console.log ("operators " + a + " / " + b + " = " + sum);
+
+var sum = a%b;
+console.log ("operators " + a + " % " + b + " = " + sum);
+
+// assignment 
+let firstValue = 20;
+let secondValue = firstValue;
+
+console.log ("the value of firstValue is: " + firstValue);
+console.log ("the value of secondValue is: " + secondValue);
+
+// Types of value
+var typeOfNumber = 4;
+var typeOfString = "I am a string";
+var typeOfBoolean = false;
+var typeOfNull = null;
+var typeOfUndefined;
+
+// numeric data (stores regular number and integers)
+console.log ("var typeOfNumber = 4; type is");
+console.log (typeof typeOfNumber);
+
+// string data (stores string of letters and symbols)
+console.log ("var typeOfString = \"I am a string\";");
+console.log(typeof typeOfString);
+
+// boolean data (stores one or the other of the binary pair true/false)
+console.log ("var typeOfBoolean = false;");
+console.log(typeof typeOfBoolean);
+
+// null data (stores the intentional absence of a value)
+console.log ("var typeOfNull = null;");
+console.log(typeof typeOfNull);
+
+//undefined data (placeholder when a variable is not set)
+console.log ("var typeOfUndefined;");
+console.log(typeof typeOfUndefined);
+
