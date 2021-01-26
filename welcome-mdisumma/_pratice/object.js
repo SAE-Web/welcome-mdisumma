f4d967353b601561ca67c0013b15595c26e9c91f
Marco Di Summa <mdisumma@yahoo.com>
2021-01-26 21:07:17 +0000

    objects

---
+++ welcome-mdisumma/_pratice/object.js
@@ -0,0 +1,17 @@
+// basic object
+var emptyObject = {};
+console.log(emptyObject)
+
+// objects data
+
+var myData = {
+    'age': 42,
+    'state' : true,
+    'name' : 'marco',
+    'surname' : 'di summa',
+    'course' : 'javaScript',
+}
+console.log(myData);
+console.log(typeof myData.number);
+console.log(typeof myData.state);
+console.log(typeof myData.name);
