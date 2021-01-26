129799141922ba2d0ecd28fabd9f8ad1ec8233aa
Marco Di Summa <mdisumma@yahoo.com>
2021-01-26 22:36:26 +0000

    Arrays

---
+++ welcome-mdisumma/_pratice/array.js
@@ -0,0 +1,6 @@
+// Arrays
+var myArray = [];
+console.log(myArray);
+
+var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday', 'Saturday', 'Sunday'];
+console.log(weekDays);
--- welcome-mdisumma/_pratice/object.js
+++ welcome-mdisumma/_pratice/object.js
@@ -1,17 +1,24 @@
 // basic object
 var emptyObject = {};
-console.log(emptyObject)
+console.log(emptyObject);
 
 // objects data
 
 var myData = {
-    'age': 42,
-    'state' : true,
-    'name' : 'marco',
-    'surname' : 'di summa',
-    'course' : 'javaScript',
-}
+    label : null,    
+    age : 42,
+    state : true,
+    name : 'marco',
+    surname : 'di summa',
+    course : 'javaScript',
+};
 console.log(myData);
-console.log(typeof myData.number);
+console.log(myData.name);
+console.log(myData["surname"]);
+console.log(typeof myData.label);
+console.log(typeof myData.age);
 console.log(typeof myData.state);
 console.log(typeof myData.name);
+
+
+
