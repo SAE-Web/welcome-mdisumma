1119b4900b5f5a0f6a179440536b57e7c33c6b60
Marco Di Summa <mdisumma@yahoo.com>
2021-01-26 15:04:36 +0000

    conditional

---
+++ welcome-mdisumma/_pratice/conditional.js
@@ -0,0 +1,14 @@
+// conditional if
+var condition = true;
+
+if (condition) {
+    console.log("the condition is true")
+};
+
+//conditional if else
+var condition = false;
+if (condition) {
+    console.log("the of condition is true")
+} else {
+    console.log("the of condition is false")
+};
