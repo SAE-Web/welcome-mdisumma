341cd2972195570c91e55fa2a10ed88f691c5780
Marco Di Summa <mdisumma@yahoo.com>
2021-01-26 22:41:43 +0000

    minor edit

--- welcome-mdisumma/_pratice/array.js
+++ welcome-mdisumma/_pratice/array.js
@@ -1,7 +1,11 @@
-// Arrays
+// Arrays preserve order and start to count from 0
 var myArray = [];
 console.log(myArray);
 
 var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'friday', 'Saturday', 'Sunday'];
 
 console.log(weekDays);
+
+var arrayOfThings = [ 0, 1, 2, 'string1', 'string2', true, false,];
+
+console.log(arrayOfThings);
