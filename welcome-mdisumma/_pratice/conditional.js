220202f1f755514abaf3700316628378176ca3ce
Marco Di Summa <mdisumma@yahoo.com>
2021-01-26 15:13:51 +0000

    conditional logic

--- welcome-mdisumma/_pratice/conditional.js
+++ welcome-mdisumma/_pratice/conditional.js
@@ -5,10 +5,24 @@ if (condition) {
     console.log("the condition is true")
 };
 
-//conditional if else
+// conditional if else
 var condition = false;
+
 if (condition) {
     console.log("the of condition is true")
 } else {
     console.log("the of condition is false")
 };
+
+// conditional logic
+let a = 5;
+let b = 4;
+let match;
+if (a == b) {
+    match = true;
+} else {
+    match = false;
+};
+
+console.log ("the items match is " + match)
+
