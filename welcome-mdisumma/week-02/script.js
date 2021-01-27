791cd8aa9216a3dc55bbaad36358f38020f77487
Marco Di Summa <mdisumma@yahoo.com>
2021-01-27 14:30:15 +0000

    morning class

---
+++ welcome-mdisumma/week-02/script.js
@@ -0,0 +1,23 @@
+let startingPoint = 1;
+const till1 = -54;
+const till2 = 56;
+
+console.log (startingPoint);
+console.log (till1-startingPoint);
+console.log (till2-startingPoint);
+
+if (till1-startingPoint<till2-startingPoint) {
+    console.log ("go to till number 1");
+} else {
+    console.log ("go to till number 2");
+}
+
+if (Math.abs(till1-startingPoint)==Math.abs(till2-startingPoint)) {
+    console.log ("hihter tills are good");
+} else {
+    if (Math.abs(till1-startingPoint)<Math.abs(till2-startingPoint)) {
+        console.log ("go to till number 1");
+    } else {
+        console.log ("go to till number 1");
+    }
+}
