bdeb23f25392cd9be7fb1a94f621ad12dd01e6ef
Marco Di Summa <mdisumma@yahoo.com>
2021-01-27 14:36:13 +0000

    min

--- welcome-mdisumma/week-02/script.js
+++ welcome-mdisumma/week-02/script.js
@@ -1,3 +1,4 @@
+//defining the variables
 let startingPoint = 1;
 const till1 = -54;
 const till2 = 56;
@@ -12,6 +13,8 @@ if (till1-startingPoint<till2-startingPoint) {
     console.log ("go to till number 2");
 }
 
+
+//Math.abs returne the absolute value of a number
 if (Math.abs(till1-startingPoint)==Math.abs(till2-startingPoint)) {
     console.log ("hihter tills are good");
 } else {
