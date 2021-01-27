049532d1796a23920425f5dd51c7e38453e55210
Marco Di Summa <mdisumma@yahoo.com>
2021-01-27 22:52:07 +0000

    copy an object

--- welcome-mdisumma/_pratice/object.js
+++ welcome-mdisumma/_pratice/object.js
@@ -21,5 +21,7 @@ console.log(typeof myData.age);
 console.log(typeof myData.state);
 console.log(typeof myData.name);
 
-
+// make a copy of an object safetly
+myData2 = JSON.parse(JSON.stringify(myData));
+console.log(myData2);
 
